5acccbc1cd047fba034b26ec5cca9cabc6911024
shahkrushang <kshah6303@gmail.com>
2023-11-22 15:58:37 +0530

    repo commit

---
+++ src/main/java/com/example/kafkaProduceDemo/KafkaProduceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaProduceDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProduceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProduceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaProduceDemo/KafkaProduceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaProduceDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProduceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
